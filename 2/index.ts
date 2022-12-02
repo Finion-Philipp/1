d6dd7795cad0b9a3ecd88041bd4e9e9626eea659
Philipp Moeller <philipp.moeller@sportalliance.com>
2022-12-02 13:37:12 +0100

    beautify day 2 task 1

--- 2/index.ts
+++ 2/index.ts
@@ -4,6 +4,56 @@ const inputFile = "./2/input";
 
 const lines = fs.readFileSync(inputFile, "utf8").split(/\r?\n/);
 
+type ElfResult = "A" | "B" | "C";
+type MeResult = "X" | "Y" | "Z";
+interface Match {
+	elf: ElfResult;
+	me: MeResult;
+}
+
+const WIN = {
+	A: "Y",
+	B: "Z",
+	C: "X",
+};
+const DRAW = {
+	A: "X",
+	B: "Y",
+	C: "Z",
+};
+const LOSE = {
+	A: "Z",
+	B: "X",
+	C: "Y",
+};
+
+function calculateTotalMatchScore(line: string): number {
+	const splitLine = line.split(" ");
+	const match = {
+		elf: splitLine[0] as ElfResult,
+		me: splitLine[1] as MeResult,
+	};
+	return calculateFigureScore(match) + calculateWinningScore(match);
+}
+
+function calculateFigureScore(match: Match): number {
+	switch (match.me) {
+		case "X":
+			return 1;
+		case "Y":
+			return 2;
+		case "Z":
+			return 3;
+	}
+}
+
+function calculateWinningScore(match: Match): number {
+	if (WIN[match.elf] === match.me) return 6;
+	if (DRAW[match.elf] === match.me) return 3;
+	if (LOSE[match.elf] === match.me) return 0;
+	return 0;
+}
+
 let totalScore1 = 0;
 
 lines.forEach((line) => {
@@ -11,40 +61,3 @@ lines.forEach((line) => {
 });
 
 console.log(`total score part 1: ${totalScore1}`);
-
-function calculateTotalMatchScore(match: string): number {
-	return calculateFigureScore(match) + calculateWinningScore(match);
-}
-
-function calculateFigureScore(match: string): number {
-	if (match.includes("X")) return 1;
-	if (match.includes("Y")) return 2;
-	if (match.includes("Z")) return 3;
-	return 0;
-}
-
-function calculateWinningScore(match: string): number {
-	switch (match) {
-		case "A X":
-			return 3;
-		case "A Y":
-			return 6;
-		case "A Z":
-			return 0;
-		case "B X":
-			return 0;
-		case "B Y":
-			return 3;
-		case "B Z":
-			return 6;
-		case "C X":
-			return 6;
-		case "C Y":
-			return 0;
-		case "C Z":
-			return 3;
-		default:
-			break;
-	}
-	return 0;
-}
