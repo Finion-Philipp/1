58b1204aa978b76eb8d85cd6a18a3ac504b24b2d
Philipp Moeller <philipp.moeller@sportalliance.com>
2022-12-03 10:53:41 +0100

    prep day 3

---
+++ 3/index.ts
@@ -0,0 +1,9 @@
+import * as fs from "fs";
+
+const inputFile = "./2/input";
+
+const lines = fs.readFileSync(inputFile, "utf8").split(/\r?\n/);
+
+lines.forEach((line) => {
+	//const splitLine = line.split(" ");
+});
