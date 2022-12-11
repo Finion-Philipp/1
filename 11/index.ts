2e85fd889498ec28d04103d725afe8cf8bcc609a
Philipp Moeller <philipp.moeller@sportalliance.com>
2022-12-11 16:03:31 +0100

    solve 11 | 2

--- 11/index.ts
+++ 11/index.ts
@@ -4,9 +4,12 @@ const inputFile = "./11/input";
 
 const lines = fs.readFileSync(inputFile, "utf8").split(/\r?\n/);
 
+let commonDivisor = 1;
+
 // TYPES & INTERFACES
 
 interface Monkey {
+	originalItems: number[];
 	items: number[];
 	operation: (a: number) => number;
 	test: (a: number) => number;
@@ -16,8 +19,10 @@ interface Monkey {
 // PARSING
 
 function parseMonkey(lines: string[]): Monkey {
+	const items = parseItems(lines[1]);
 	return {
-		items: parseItems(lines[1]),
+		originalItems: [...items],
+		items,
 		operation: parseOperation(lines[2]),
 		test: parseTest(lines.slice(3)),
 		inspectionCount: 0,
@@ -39,6 +44,7 @@ function parseOperation(line: string): (a: number) => number {
 
 function parseTest(lines: string[]): (a: number) => number {
 	const divisor = Number(lines[0].match(/\d+/g));
+	commonDivisor *= divisor;
 	const trueMonkey = Number(lines[1].match(/\d+/g));
 	const falseMonkey = Number(lines[2].match(/\d+/g));
 	return (a: number) => {
@@ -55,6 +61,8 @@ for (let i = 0; i < filteredLines.length; i += 6) {
 }
 
 // TURNS AND ROUNDS
+
+// PART 1
 
 function round() {
 	monkeys.forEach((monkey) => turn(monkey));
@@ -75,3 +83,34 @@ for (let i = 0; i < 20; i++) {
 }
 let result1 = monkeys.map((it) => it.inspectionCount).sort((a, b) => b - a);
 console.log(result1[0] * result1[1]);
+
+// PART 2
+
+function reset() {
+	for (let monkey of monkeys) {
+		monkey.items = [...monkey.originalItems];
+		monkey.inspectionCount = 0;
+	}
+}
+
+function round2() {
+	monkeys.forEach((monkey) => turn2(monkey));
+}
+
+function turn2(monkey: Monkey) {
+	monkey.items.forEach((item) => {
+		monkey.inspectionCount++;
+		let worryLevel = monkey.operation(item);
+		worryLevel = worryLevel % commonDivisor;
+		monkeys[monkey.test(worryLevel)].items.push(worryLevel);
+	});
+	monkey.items = [];
+}
+
+reset();
+
+for (let i = 0; i < 10000; i++) {
+	round2();
+}
+let result2 = monkeys.map((it) => it.inspectionCount).sort((a, b) => b - a);
+console.log(result2[0] * result2[1]);
