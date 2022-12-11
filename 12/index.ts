62173e9f70facf05c4f83364fbebdb6bdd398672
Philipp Moeller <philipp.moeller@sportalliance.com>
2022-12-11 16:05:00 +0100

    prep 12

---
+++ 12/index.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+
+const inputFile = "./12/input";
+
+const lines = fs.readFileSync(inputFile, "utf8").split(/\r?\n/);
+
+// TYPES & INTERFACES
+
+// PARSING
+
+// PART 1
+
+// PART 2
